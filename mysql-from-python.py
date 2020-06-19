658918e92874dbefd626e52f15c272996598f039
MauRuRo <m.rubenderoos@gmail.com>
2020-06-19 19:38:26 +0000

    save excercise

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 import pymysql
 
 # get username cloud9 workspace
@@ -15,10 +16,14 @@ connection = pymysql.connect(host='localhost',
 try:
     # Run a query
     with connection.cursor() as cursor:
-        sql = "select * from Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        list_of_names = ["Fred", "Fred"]
+        # prepare a string with the same number of placeholders as in list of names
+        format_strings = ','.join(['%s']*len(list_of_names))
+        cursor.execute("delete from Friends where name in ({})".format(format_strings), list_of_names)
+        connection.commit()
+    # Note that the above will display a waring (not error) if the
+    # table already exists.
 finally:
     # Close the connection, regardless of whether the above was succesful
     connection.close()
+
